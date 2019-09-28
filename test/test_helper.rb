ee9317a1aea4dcc73305ba04354ce3de5e7c8818
Hao Ling Zhang <hzhang@utbm.fr>
2019-09-28 16:09:26 +0200

    test08

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,25 +1,14 @@
+ENV['RAILS_ENV'] ||= 'test'
 
+require 'simplecov'
+SimpleCov.start
 
+require_relative '../config/environment'
+require 'rails/test_help'
 
-require 'test_helper'
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
 
-class StudentTest < ActiveSupport::TestCase
-
-  test "should have the necessary required validators" do
-
-    student = student.new
-
-    assert_not student.valid?
-
-    assert_equal [:name, :last_name], student.errors.keys
-
-    student.name = "Gerard"
-
-    student.last_name= "Bouchard"
-
-    assert student.valid?
-
-  end
-
+  # Add more helper methods to be used by all tests here...
 end
-
